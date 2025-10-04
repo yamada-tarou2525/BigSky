c653d2764fd75514abe4fbb7647c6266a04ff1a8
yamada-tarou2525 <jakku1582@gmail.com>
2025-10-04 20:42:57 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -2,6 +2,7 @@ import { Player } from "./player.js";
 import { Enemy } from "./enemy.js";
 import { Bullet } from "./bullet.js";
 import { InputHandler } from "./input.js";
+import { EnemyBullet } from "./EnemyBullet.js";
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
