60ff5a82d76b9a7cac2ca228d7cf8766270c7195
kotokoto381 <ayayui0612@icloud.com>
2025-10-04 19:46:52 +0900

    ｔｔｔｔｔｔｔｔ

---
+++ js/input.js
@@ -0,0 +1,12 @@
+export class InputHandler {
+  constructor() {
+    this.keys = {};
+
+    window.addEventListener("keydown", e => {
+      this.keys[e.key] = true;
+    });
+    window.addEventListener("keyup", e => {
+      this.keys[e.key] = false;
+    });
+  }
+}
